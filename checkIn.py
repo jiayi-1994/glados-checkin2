89a2995251d9e24e66be6ed6138a41ac34083127
Fredric Goldrich <120161630+jiayi-1994@users.noreply.github.com>
2024-03-22 11:02:09 +0800

    Update checkIn.py

--- checkIn.py
+++ checkIn.py
@@ -1,23 +1,16 @@
-import requests
+import http.client
 
-def start():
-    url = "https://dujiao.huiyan-ai.cn/api/user-signIn"
-    headers = {
-        "Authorization": "Bearer eyJUeXBlIjoiSnd0IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJyb290IjpmYWxzZSwibmFtZSI6bnVsbCwiYXZhdGFyIjoiaHR0cHM6Ly9taW5pb3MuaHVpeWFuLWFpLmNuL2ltYWdlL3RvdXhpYW5nLnBuZyIsImlkIjoxNzcwNjQyMjYzMjc2MzA2NDM0LCJleHAiOjE3MTE1OTM3ODcsInR5cGUiOm51bGx9.ZKI_uYg0moQpygGjkN5OMuADNlhoXqtwmByZZKXQLww",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "key1": "value1",
-        "key2": "value2"
-    }
-
-    response = requests.post(url, headers=headers, json=data)
-
-    print(response.status_code)
-    print(response.json())
-    
-def main_handler(event, context):
-      return start()
-
-if __name__ == '__main__':
-    start()
+conn = http.client.HTTPSConnection("dujiao.huiyan-ai.cn")
+payload = ''
+headers = {
+   'Authorization': 'Bearer eyJUeXBlIjoiSnd0IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJyb290IjpmYWxzZSwibmFtZSI6bnVsbCwiYXZhdGFyIjoiaHR0cHM6Ly9taW5pb3MuaHVpeWFuLWFpLmNuL2ltYWdlL3RvdXhpYW5nLnBuZyIsImlkIjoxNzcwNjQyMjYzMjc2MzA2NDM0LCJleHAiOjE3MTE1OTM3ODcsInR5cGUiOm51bGx9.ZKI_uYg0moQpygGjkN5OMuADNlhoXqtwmByZZKXQLww',
+   'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+   'Accept': '*/*',
+   'Host': 'dujiao.huiyan-ai.cn',
+   'Connection': 'keep-alive',
+   'Cookie': 'sl-session=2kD1TPRD/mV+hVoVHI4FRA=='
+}
+conn.request("POST", "/api/user-signIn", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
