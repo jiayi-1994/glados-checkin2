49a2c72931679279b219f1ef9ea76580085d4cfc
Fredric Goldrich <120161630+jiayi-1994@users.noreply.github.com>
2024-03-22 10:59:14 +0800

    Update checkIn.py

--- checkIn.py
+++ checkIn.py
@@ -1,16 +1,23 @@
 import requests
 
-url = "https://dujiao.huiyan-ai.cn/api/user-signIn"
-headers = {
-    "Authorization": "Bearer eyJUeXBlIjoiSnd0IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJyb290IjpmYWxzZSwibmFtZSI6bnVsbCwiYXZhdGFyIjoiaHR0cHM6Ly9taW5pb3MuaHVpeWFuLWFpLmNuL2ltYWdlL3RvdXhpYW5nLnBuZyIsImlkIjoxNzcwNjQyMjYzMjc2MzA2NDM0LCJleHAiOjE3MTE1OTM3ODcsInR5cGUiOm51bGx9.ZKI_uYg0moQpygGjkN5OMuADNlhoXqtwmByZZKXQLww",
-    "Content-Type": "application/json"
-}
-data = {
-    "key1": "value1",
-    "key2": "value2"
-}
+def start():
+    url = "https://dujiao.huiyan-ai.cn/api/user-signIn"
+    headers = {
+        "Authorization": "Bearer eyJUeXBlIjoiSnd0IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJyb290IjpmYWxzZSwibmFtZSI6bnVsbCwiYXZhdGFyIjoiaHR0cHM6Ly9taW5pb3MuaHVpeWFuLWFpLmNuL2ltYWdlL3RvdXhpYW5nLnBuZyIsImlkIjoxNzcwNjQyMjYzMjc2MzA2NDM0LCJleHAiOjE3MTE1OTM3ODcsInR5cGUiOm51bGx9.ZKI_uYg0moQpygGjkN5OMuADNlhoXqtwmByZZKXQLww",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "key1": "value1",
+        "key2": "value2"
+    }
 
-response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data)
 
-print(response.status_code)
-print(response.json())
+    print(response.status_code)
+    print(response.json())
+    
+def main_handler(event, context):
+      return start()
+
+if __name__ == '__main__':
+    start()
