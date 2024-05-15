390c8d49fdc2fdc6a066d25b513d8d097ec7914e
Fredric Goldrich <120161630+jiayi-1994@users.noreply.github.com>
2024-05-15 09:41:01 +0800

    Update checkIn.py

--- checkIn.py
+++ checkIn.py
@@ -3,7 +3,7 @@ import http.client
 conn = http.client.HTTPSConnection("dujiao.huiyan-ai.cn")
 payload = ''
 headers = {
-   'Authorization': 'Bearer eyJUeXBlIjoiSnd0IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJyb290IjpmYWxzZSwibmFtZSI6bnVsbCwiYXZhdGFyIjoiaHR0cHM6Ly9taW5pb3MuaHVpeWFuLWFpLmNuL2ltYWdlL3RvdXhpYW5nLnBuZyIsImlkIjoxNzcwNjQyMjYzMjc2MzA2NDM0LCJleHAiOjE3MTE1OTM3ODcsInR5cGUiOm51bGx9.ZKI_uYg0moQpygGjkN5OMuADNlhoXqtwmByZZKXQLww',
+   'Authorization': 'Bearer eyJUeXBlIjoiSnd0IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJyb290IjpmYWxzZSwibmFtZSI6IjE3NjA2NjcxMTAzIiwiYXZhdGFyIjoiaHR0cHM6Ly9taW5pb3MuaHVpeWFuLWFpLmNuL2ltYWdlL3RvdXhpYW5nLnBuZyIsImlkIjoxNzkwNTU3Njk4MjEyODU5OTA1LCJleHAiOjE3MTYzNDE5OTcsInR5cGUiOjF9.jDua3b2-V2lwUFctuAtNmfCR29jSyJ2urFusD8xgtMc',
    'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
    'Accept': '*/*',
    'Host': 'dujiao.huiyan-ai.cn',
